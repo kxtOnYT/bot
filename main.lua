8bbf89235ffa5be96d1ac514ef110dfd397a7289
kxtOnYT <104738419+kxtOnYT@users.noreply.github.com>
2025-11-23 20:40:59 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,7 +9,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/
 local MESSAGES = {
     "join /hTTbCWby | ",
     "please join im desperate | ", --1401980757135724674
-    "KORBLOX GIVEAWAY IN #GWS  | ", -- 1290376267866308750 
+    "Only chadlites join /hTTbCWby  | ", -- 1290376267866308750 
     "/hTTbCWby | " -- 1424087959103209675
 }
 local MESSAGE_INTERVAL    = 3     -- seconds between messages / target switches
