afbab6d4cbdd1f725d392336d758c356302bd45e
kxtOnYT <104738419+kxtOnYT@users.noreply.github.com>
2025-11-25 21:14:37 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,9 +8,10 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/
 -- SETTINGS
 local MESSAGES = {
     "join /hTTbCWby | ",
-    "grim can't moderate 😂 | ", --1401980757135724674
+    "mods cant ban me | ", --1401980757135724674
     "custom messages in /hTTbCWby  | ",
     "Join to get your own bot  | ",-- 1290376267866308750 
+    "improve never stops 😂  | ",-- 1290376267866308750 
     "/hTTbCWby | " -- 1424087959103209675
 }
 local MESSAGE_INTERVAL    = 3     -- seconds between messages / target switches
